fadf26369827f64b6a24d946b12875ca744015b3
lcckkkhaha <lcckkkhaha@163.com>
2020-10-01 19:32:20 +0200

    modify debug example

--- Heuristic selection/src/debug_example.py
+++ Heuristic selection/src/debug_example.py
@@ -1,28 +1,36 @@
 from horn_dataset import train_on_graphs
 
 """
-This will read train, valid, and test data from trainData, validData, and testData files in ../benchmarks/debug-examples/
+This will read train, valid, and test data from trainData, validData, and testData files in ../temp-extract-trainData-datafold/
 
-In every data folder such as trainData, you can find two types of files i.e. file_name.gv and file_name.arguments.
-A pair of them is one graph input. You can manually copy other .gv and .arguments file pairs from other folder in benchmarks to form different dataset.
+.layerHornGraph.JSON files contain all information of the graph.
+.layerHornGraph.gv files can be rend as .jpg file to visualize the graph
 
-For a simplest test, you can use one pair of files for three datafolds. 
+For a simplest test, you can use one file for three datafolds. 
+To reproduce memory out problem, you can replicate similar graphs for many times.
+
+The training logs can be find in src/trained_model folder
 
 """
 
 def main():
     benchmark_list = []
-    benchmark_list.append(["../benchmarks/debug-examples/", None, None, None])
+    benchmark_list.append(["../benchmarks/temp-extract-trainData-datafold/"])
     force_read = True
+    from_json=True
+    form_label=True
+    json_type=".layerHornGraph.JSON"
     file_type = ".smt2"
-    #label = "occurrence"
-    label = "rank"
+    label = "occurrence"
+    #label = "rank"
     #label = "argument_identify"
     #label = "argument_identify_no_batchs"
     #label = "control_location_identify"
 
     for benchmark in benchmark_list:
-        train_on_graphs(benchmark_name=benchmark[0][len("../benchmarks/"):-1], label=label, force_read=force_read, train_n_times=1,
-                        path=benchmark[0], file_type=file_type)
+        train_on_graphs(benchmark_name=benchmark[0][len("../benchmarks/"):-1], label=label, force_read=force_read,
+                        train_n_times=1,
+                        path=benchmark[0], file_type=file_type, form_label=form_label, from_json=from_json,
+                        json_type=json_type)
 
 main()
