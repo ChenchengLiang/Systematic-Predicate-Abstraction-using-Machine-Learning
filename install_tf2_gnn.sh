d4e7b6360aca99be0838900a0cbea51f558b9714
lcckkkhaha <lcckkkhaha@163.com>
2020-11-07 00:22:00 +0100

    install tf2_gnn and other dependent libraries

---
+++ install_tf2_gnn.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git clone https://github.com/ChenchengLiang/tf2-gnn
+cd tf2-gnn
+git checkout horn_graph_with_hyperedge
+p install -e ./
+
+pip install networkx
+pip install graphviz
+pip install genism
+pip install nltk
+pip install matplotlib
+pip install numba
+
