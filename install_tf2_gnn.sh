c5bb9c0f3ee346ffe6312bd42c52d205c81cfac2
Chencheng Liang <lcckkkhaha@163.com>
2021-05-18 18:12:45 +0200

    Update install_tf2_gnn.sh

--- install_tf2_gnn.sh
+++ install_tf2_gnn.sh
@@ -2,19 +2,19 @@
 git clone https://github.com/ChenchengLiang/tf2-gnn
 cd tf2-gnn
 git checkout horn_graph_with_hyperedge
-pip install -e ./
+pip3 install -e ./
 
-pip install networkx
-pip install graphviz
-pip install gensim
-pip install nltk
-pip install matplotlib
-pip install numba
-pip install seaborn
-pip install sklearn
-pip install multiprocessing
-pip install json
-pip install subprocess
-pip install numpy
-pip install tensorflow
+pip3 install networkx
+pip3 install graphviz
+pip3 install gensim
+pip3 install nltk
+pip3 install matplotlib
+pip3 install numba
+pip3 install seaborn
+pip3 install sklearn
+pip3 install multiprocessing
+pip3 install json
+pip3 install subprocess
+pip3 install numpy
+pip3 install tensorflow
 
